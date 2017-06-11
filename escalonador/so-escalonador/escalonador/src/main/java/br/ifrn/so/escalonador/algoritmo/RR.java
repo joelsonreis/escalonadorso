3f10a171382fcc3b5b3d7f38a7b8c42a765743c9
JoelsonBatista <joelson_batista@outlook.com>
2017-06-10 23:13:07 -0300

    acrescetando a classe de Round Robin

---
+++ escalonador/so-escalonador/escalonador/src/main/java/br/ifrn/so/escalonador/algoritmo/RR.java
@@ -0,0 +1,36 @@
+package br.ifrn.so.escalonador.algoritmo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import br.ifrn.so.escalonador.Principal;
+import br.ifrn.so.escalonador.model.Processo;
+import br.ifrn.so.escalonador.model.thread.ProcessoThreadRR;
+
+/**
+ * Round Robin
+ * 
+ * @author joelsonreis
+ */
+public class RR extends Algoritmo implements ActionListener {
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public void actionPerformed(ActionEvent e) {
+		if (Principal.processamento != null)
+			Principal.processamento.stop();
+
+		Principal.processosAptos = new ArrayList<Processo>();
+		Principal.processosEmExecucao = new ArrayList<Processo>();
+		criarProcessos();
+		montarNucleos();
+		montarProcessos();
+		
+		Principal.processamento = new ProcessoThreadRR();
+		Principal.processamento.start();
+	}
+}
+
+public class RR {
+
+}
